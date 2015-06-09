385de4b4188012fc4e90247280f317b2899e9a0b
Craig Franklin <craigfranklin@Craigs-MacBook-Pro-2.local>
2015-06-09 15:25:51 -0400

    Exercise 3: added a .gets for user age. Completed.

--- exercise3.rb
+++ exercise3.rb
@@ -4,3 +4,11 @@ name = gets.chomp
 name.capitalize!
 puts "Hi #{name}!"
 
+puts "I know I shouldn't ask, but how old are you?"
+age = gets.chomp
+age = age.to_i
+puts "Oh, wow. #{age}! You're younger than I thought!"
+current_year = 2015
+birth_year = current_year - age
+puts "So that means you were born around #{birth_year}? Cool!"
+# Only problem I see with this method it does not take into account the calender months. So some discrepency is to be expected. 
